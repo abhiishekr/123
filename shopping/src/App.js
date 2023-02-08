a13a73b43c5338a34dcfbdd56e1598a9417e10f0
abhiishekr <raoabhishek0007@gmail.com>
2023-02-08 23:24:19 +0530

    commit

---
+++ shopping/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Product from './components/Product';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Product />
+      <Footer />      
+    </div>
+  );
+}
+
+export default App;
